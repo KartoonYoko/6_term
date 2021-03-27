497fe651f8f27e537ded60a0f4ada374b08e06e1
Vladislav <crashed9001@gmail.com>
2021-03-27 21:41:23 +0900

    just commit

--- Computer-graphics/Computer-graphics/lab 6/index.js
+++ Computer-graphics/Computer-graphics/lab 6/index.js
@@ -1,25 +1,50 @@
 'use strict';
 
+function init(){
+    const canvas = document.querySelector('.canvas');
+    let ctx = canvas.getContext('2d');
 
-// initializing canvas
-const canvas = document.querySelector('.canvas');
-const widthDocument = document.documentElement.clientWidth;
-const heightDocument = document.documentElement.clientHeight;
-canvas.style.width = widthDocument + 'px';
-canvas.style.height = heightDocument - 100 + 'px';
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // clear canvas
 
-const ctx = canvas.getContext('2d');
-ctx.lineWidth = 1;
-// changing coordinates
-let transX = canvas.width * 0.5;
-let transY = canvas.height * 0.5;
-ctx.translate(transX, transY);
+    let cube = new Cube(ctx, 100, 'rgb(0, 0, 139, 1)', 'rgb(255, 0, 0, 1)');
+    cube.move(new Point3D(-50,-50,-50));
+      
+    let centerScreen = new Point2D(canvas.width / 2, canvas.height / 2);
+    cube.put(centerScreen);
 
-let length = 40;
-let diff = 15;
-let x = -20;
-let y = -20;
-let cube = new Cube(length, diff, x, y);
+    let timer = setInterval
+    (
+        nextFrame, 
+        1000 / 60,
+        ctx,
+        canvas,
+        cube,
+        centerScreen
+    );
+
+  }
+
+function nextFrame(ctx, canvas, cube, cen6terScreen){
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';  
+    ctx.fillRect(0, 0, canvas.width, canvas.height);  // clear canvas
+    const btnX = document.querySelector('.checkbox1');
+    const btnY = document.querySelector('.checkbox2');
+    const btnZ = document.querySelector('.checkbox3');
+    if (btnX.checked) {
+        cube.rotate('x', 0.4);
+    }
+    if (btnY.checked) {
+        cube.rotate('y', 0.6);
+    }
+    if (btnZ.checked) {
+        cube.rotate('z', 0.3);
+    }
+    ctx.fillStyle = 'rgba(50, 50, 200, 1)';
+    ctx.strokeStyle = 'rgba(60, 60, 210, 1)';
+    cube.put(centerScreen);
+}
 
 
-cube.draw(ctx);
+init();
+
