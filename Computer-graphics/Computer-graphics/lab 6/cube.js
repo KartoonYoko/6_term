62179ab28083c21fe033ae082e59483881028554
Vladislav <crashed9001@gmail.com>
2021-03-27 17:02:20 +0900

    just commit

--- Computer-graphics/Computer-graphics/lab 6/cube.js
+++ Computer-graphics/Computer-graphics/lab 6/cube.js
@@ -21,31 +21,31 @@ class Cube{
      * @param {int} x 
      * @param {int} y 
      * @param {int} diff - насколько далеко задний квадра от переднего
-     * @param {int} length - расстояние от переднего квадрата до заднего
+     * @param {int} length - расстояние между точками
      */
-    constructor(x, y, diff, length){
-        
+    constructor(length, diff, x = -20, y = -20 ){
+        let n = 8;
+
         this.arr = [];
 
         for (let i = 0; i < n; i++){
-            arr.push(new Point);
+            this.arr.push(new Point());
         }
 
         this.length = length || 40;
         this.diff = diff || 15;
-        this.x = x || -20;
-        this.y = y || -20;
-        arr[0].setPoint(x, y);
-        arr[1].setPoint(x, y + length)
-        arr[2].setPoint(x + length, y + length);
-        arr[3].setPoint(x + length, y);
+
+        this.arr[0].setPoint(x, y);
+        this.arr[1].setPoint(x, y + length);
+        this.arr[2].setPoint(x + length, y + length);
+        this.arr[3].setPoint(x + length, y);
 
         let x1 = x + diff;
         let y1 = y - diff;
-        arr[4].setPoint(x1, y1);
-        arr[5].setPoint(x1, y1 + length)
-        arr[6].setPoint(x1 + length, y1 + length);
-        arr[7].setPoint(x1 + length, y1);
+        this.arr[4].setPoint(x1, y1);
+        this.arr[5].setPoint(x1, y1 + length);
+        this.arr[6].setPoint(x1 + length, y1 + length);
+        this.arr[7].setPoint(x1 + length, y1);
     }
 
     turnX(){
@@ -54,10 +54,42 @@ class Cube{
 
     /**
      * 
+     * Рисует куб
+     * 
      * @param {context} ctx - context of canvas 
      */
     draw(ctx){
+        ctx.fillStyle = 'rgb(200, 0, 0)';
+        //draw face square
+        ctx.beginPath();
+        ctx.moveTo(this.arr[0].x, this.arr[0].y);
+        for (let i = 1; i < 4; i++){
+            ctx.lineTo(this.arr[i].x, this.arr[i].y);
+        }
+        ctx.lineTo(this.arr[0].x, this.arr[0].y);
+        ctx.closePath();
+        ctx.fill();  
 
+        //draw back square
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.beginPath();
+        ctx.moveTo(this.arr[4].x, this.arr[4].y);
+        for (let i = 4; i < 8; i++){
+            ctx.lineTo(this.arr[i].x, this.arr[i].y);
+        }
+        ctx.lineTo(this.arr[4].x, this.arr[4].y);
+        ctx.closePath();
+        ctx.fill();
+        
+        // draw lines
+        ctx.strokeStyle = 'rgb(0, 0, 0)';
+        ctx.beginPath();
+        for (let i = 0; i < 4; i++){
+            ctx.moveTo(this.arr[i].x, this.arr[i].y);
+            ctx.lineTo(this.arr[i + 4].x, this.arr[i + 4].y);
+        }
+        ctx.closePath();
+        ctx.stroke();
     }
 
 }
