f1a13461916ac14fb3d1c3d5eea37fc5eec8edc9
KartoonYoko <crashed9001@gmail.com>
2021-04-17 13:09:47 +0900

    added zalivka zatravkoi from anglii

--- Computer-graphics/Computer-graphics/lab 9/index.js
+++ Computer-graphics/Computer-graphics/lab 9/index.js
@@ -17,116 +17,6 @@ class Point2D{
     }
 }
 
-/**
- * 
- * @returns {array} array of Point2D's
- */
-function getData(){
-    let point1X = document.getElementById('point1x');
-    let point2X = document.getElementById('point2x');
-    let point3X = document.getElementById('point3x');
-
-    let point1Y = document.getElementById('point1y');
-    let point2Y = document.getElementById('point2y');
-    let point3Y = document.getElementById('point3y');
-
-    let arr = [];
-    arr.push(new Point2D(parseInt(point1X.value), parseInt(point1Y.value)));
-    arr.push(new Point2D(parseInt(point2X.value), parseInt(point2Y.value)));
-    arr.push(new Point2D(parseInt(point3X.value), parseInt(point3Y.value)));
-
-    return arr;
-}
-
-/**
- * Рисует треугольник
- * 
- * @param {*} ctx 
- * @param {Point2D} p1 
- * @param {Point2D} p2 
- * @param {Point2D} p3 
- * @param {*} color 
- */
-function drawTriangle(ctx, p1, p2, p3, color = 'rgb(0, 250, 250)'){
-    ctx.strokeStyle = color;
-    ctx.beginPath();
-
-    ctx.moveTo(p1.x, p1.y);
-    ctx.lineTo(p2.x, p2.y);
-    ctx.lineTo(p3.x, p3.y);
-    ctx.lineTo(p1.x, p1.y);
-
-    // ctx.moveTo(p1.x + 1, p1.y);
-    // ctx.lineTo(p2.x + 1, p2.y);
-    // ctx.lineTo(p3.x + 1, p3.y);
-    // ctx.lineTo(p1.x + 1, p1.y);
-
-    // ctx.moveTo(p1.x + 2, p1.y);
-    // ctx.lineTo(p2.x + 2, p2.y);
-    // ctx.lineTo(p3.x + 2, p3.y);
-    // ctx.lineTo(p1.x + 2, p1.y);
-    ctx.stroke();
-}
-
-/**
- * Рисует квадрат
- * 
- * @param {*} ctx 
- * @param {Point2D} p1 
- * @param {Point2D} p2 
- * @param {Point2D} p3 
- * @param {Point2D} p4 
- * @param {String} color 
- */
-function drawSquare(ctx, p1, p2, p3, p4, color = 'rgb(0, 250, 250)'){
-    ctx.strokeStyle = color;
-    ctx.beginPath();
-
-    ctx.moveTo(p1.x, p1.y);
-    ctx.lineTo(p2.x, p2.y);
-    ctx.lineTo(p3.x, p3.y);
-    ctx.lineTo(p4.x, p4.y);
-    ctx.lineTo(p1.x, p1.y);
-    ctx.stroke();
-}
-
-/**
- * 
- * @param {*} ctx 
- * @param {Point2D} p1 
- * @param {Point2D} p2 
- * @param {Point2D} p3 
- */
-function paintOver(ctx, p1, p2, p3, color = 'rgb(0, 250, 250)'){
-
-    // k = (p3.y - p1.y) / (p3.x - p1.x)
-    let k1 = (p2.x - p1.x) / (p2.y - p1.y);
-    let k2 = (p3.x - p1.x) / (p3.y - p1.y);
-
-    ctx.strokeStyle = color;
-    ctx.beginPath();
-
-    let x1 = 0;
-    let x2 = 0;
-    for (let y = p1.y; y < p2.y; y++){
-        x1 = p1.x + (y - p1.y) * k1;
-        x2 = p1.x + (y - p1.y) * k2;
-        ctx.moveTo(x1, y);
-        ctx.lineTo(x2, y);
-    }
-    ctx.stroke();
-
-    let k3 = (p2.x - p3.x) / (p2.y - p3.y);
-    let x = x2;
-    ctx.beginPath();
-    for (let y = p2.y; y < p3.y; y++){
-        x1 = x + (y - p2.y) * k2;
-        x2 = p2.x + (y - p2.y) * k3;
-        ctx.moveTo(x1, y);
-        ctx.lineTo(x2, y);
-    }
-    ctx.stroke();
-}
 
 /**
  * Закрасить пиксель
@@ -256,9 +146,9 @@ class Stack{
     let point = null;
     let count = 0;
     stack.push(p);
-    while((stack.size() != 0) && (count <= 100)){
+    while((stack.size() != 0) && (count <= 250000)){
         pixel = stack.pop();
-        console.log(pixel);
+        
         if (checkPixelColor(ctx, pixel, color)){
             
             setPixel(ctx, pixel, paintColor[0]);
@@ -289,6 +179,7 @@ class Stack{
            
         }
     }
+    console.log(count);
 }
 
 /**
@@ -366,46 +257,10 @@ const CANVAS_COLOR = 'rgb(255, 255, 255)';
 const DRAW_COLOR = 'rgb(0, 250, 250)';
 
 const canvas = document.querySelector('.canvas');
-canvas.width = 1000;
-canvas.height = 1000;
+canvas.width = 500;
+canvas.height = 500;
 const ctx = canvas.getContext('2d');
 let colors = [];
-
-const btnStart = document.querySelector('.btn');
-btnStart.addEventListener('click', () => {
-    let arr = getData();
-
-    ctx.fillStyle = CANVAS_COLOR;
-    ctx.fillRect(0, 0, canvas.width, canvas.height); // clear canvas
-
-    colors = [];
-
-    drawTriangle(ctx, ...arr);
-    colors.push(getColorOfPixel(ctx, arr[0]));
-    colors.push('rgb(0, 250, 250)');
-
-    // drawSquare(
-    //     ctx, 
-    //     new Point2D(1, 1),
-    //     new Point2D(100, 1),
-    //     new Point2D(100, 100),
-    //     new Point2D(1, 100)
-    // );
-    // colors.push(getColorOfPixel(ctx, new Point2D(1, 1)));
-    // colors.push(getColorOfPixel(ctx, new Point2D(50, 100)));
-    // colors.push('rgb(0, 250, 250)');
-    
-});
-
-const btnPaint = document.querySelector('.btn-paint');
-btnPaint.addEventListener('click', (e) => {
-    let arr = getData();
-    // paintOver(ctx, ...arr);
-
-    // zatravka4(ctx, new Point2D(50, 40), colors);
-    zatravka4Color(ctx, new Point2D(50, 40), colors, "rgb(255, 255, 255)");
-    // setPixel(ctx, new Point2D(50, 40));
-});
 
 window.addEventListener('click', function (e) {
     if (e.ctrlKey){
@@ -415,8 +270,7 @@ window.addEventListener('click', function (e) {
 
         colors.push(DRAW_COLOR);
         setPixel(ctx, new Point2D(e.pageX, e.pageY), "rgb(0, 0, 0)");
-        zatravka4Color(ctx, new Point2D(e.pageX, e.pageY), colors, "rgb(0, 0, 0)");
-        setPixel(ctx, new Point2D(e.pageX, e.pageY), "rgb(255, 0, 0)");
+        zatravka8Color(ctx, new Point2D(e.pageX, e.pageY), colors, "rgb(0, 0, 0)");
     }
 });
 
@@ -454,3 +308,20 @@ canvas.addEventListener("mouseup", function(e){
 });
 
 
+let isMax = false;
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            isMax = true;
+        } 
+        if (width <= 1) isMax = false;
+        if (isMax) width--;
+        else width++;
+        elem.style.width = width + "%";
+    }
+}
+
+move();
