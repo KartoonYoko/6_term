5df5c525941a23ba7327bee0657c6622b3b3ebee
KartoonYoko <crashed9001@gmail.com>
2021-05-10 21:18:10 +0900

    asd

--- Experimental data processing/lab 7/index.py
+++ Experimental data processing/lab 7/index.py
@@ -3,22 +3,65 @@ import math
 import numpy as np
 
 # variant 17
+# (5.81, 4.27)
+# 7 and 8
 
 
-def lagranz(x, y, t):
+def lagranz(x1, x2, y, t1, t2):
+    """
+    Интерполяционный многочлен Лагранжа для 2 переменых
+    :param: list: x1
+        список значений аргумента
+    :param: list: x2
+        список значений второго аргумента
+    :param: matrix: y
+        значения функции
+    :param: float: t1
+        значения первого аргумента, для которого вычесляется значение функции
+    :param: float: t2
+        значения второго аргумента, для которого вычесляется значение функции
+    """
+
     z = 0
-    for j in range(len(y)):
-        p1 = 1
-        p2 = 1
-        for i in range(len(x)):
-            if i == j:
-                p1 = p1*1
-                p2 = p2*1
-            else:
-                p1 = p1*(t-x[i])
-                p2 = p2*(x[j]-x[i])
-        z = z+y[j]*p1/p2
+    for k in range(len(y)):
+        for j in range(len(y[0])):
+            p11 = 1
+            p12 = 1
+            p21 = 1
+            p22 = 1
+            for i in range(len(x1)):
+                if i != j:
+                    p11 *= t1-x1[i]
+                    p12 *= x1[j]-x1[i]
+            for i in range(len(x2)):
+                if i != k:
+                    p21 *= t2-x2[i]
+                    p22 *= x2[k]-x2[i]
+
+            z += y[k][j] * (p11 / p12) * (p21 / p22)
     return z
+
+
+def measurment_error(const_x1, const_x2, x1, x2, t1, t2):
+    """
+    поиск погрешности
+    """
+
+    def fac(n):
+        if n == 0:
+            return 1
+        return fac(n - 1) * n
+
+    res = 0
+    pr = 1
+    for i in range(len(x1)):
+        pr *= math.fabs(t1 - x1[i])
+    res += const_x1 / fac(len(x1) + 1) * pr
+    pr = 1
+    for i in range(len(x2)):
+        pr *= math.fabs(t2 - x2[i])
+    res += const_x2 / fac(len(x2) + 1) * pr
+    return res
 
 
 x = np.array([5.45, 5.6, 5.75, 5.9, 6.05, 6.2, 6.35, 6.5, 6.65], dtype=float)
@@ -33,13 +76,70 @@ tab = [
     [3.33, 3.33, 2.43, 6.54, 5.34, 5.34, 4.54, 7.54, 5.43],
     [3.54, 5.34, 6.54, 4.54, 5.54, 4.40, 6.76, 4.34, 4.76]
 ]
-print(tab)
+
+tab1 = 5.81
+tab2 = 4.27
+x_pr = 7
+y_pr = 8
+print("Значение в точке", tab1, tab2, ":", lagranz(x, y, tab, tab1, tab2))
+print("Погрешность:", measurment_error(x_pr, y_pr, x, y, tab1, tab2))
+
+print("Second task ------------------------------------------------------------------------------------------------")
+x = np.array([7.31, 7.65, 8.12, 8.67, 8.99, 9.34, 9.89, 5.34, 5.87, 6.88, 7.13, 7.45, 7.89, 8.41, 8.95], dtype=float)
+y = np.array([7.34, 5.54, 6.24, 3.76, 4.98, 3.67, 8.35, 7.55, 7.45, 6.54, 4.34, 6.76, 3.43, 2.65, 4.34], dtype=float)
+z = np.array([8.98, 8.54, 8.76, 8.45, 4.34, 5.34, 3.45, 9.34, 9.54, 9.34, 3.76, 4.34, 3.34, 5.76, 4.56], dtype=float)
 
 
-def bernstein(n, m):
-    '''
-        двумерный многочлен Бернштейна
-    '''
+def fac(n):
+    if n == 0:
+        return 1
+    return fac(n - 1) * n
+
+
+def combinations(k, n):
+    """
+    Сочетания
+    """
+    return fac(n) / (fac(k) * fac(n - k))
+
+
+def bernstein(x1, x2, z, x, y):
+    """
+    Бернштейн для 2 размерности
+    """
+    n = len(x1)
+    m = len(x2)
+    a = np.min(x1)
+    b = np.max(x1)
+    c = np.min(x2)
+    d = np.max(x2)
+    res = 0
     for k in range(n):
-        for k in range(m):
-            pass
+        buf = 0
+        for l in range(m):
+            # z(Xk, Yk) TODO откуда или как посчитать
+            buf = combinations(k, n) * combinations(l, m)
+            buf *= ((x - a) / (b - a)) ** k * (1 - (x - a) / (b - a)) ** (n - k)
+            buf *= ((y - c) / (d - c)) ** l * (1 - ((y - c) / (d - c))) ** (m - l)
+            res += buf
+    return res
+
+
+# Xk = a + (b - a) / n * k; k = 0, n
+# Yl = c + (d - c) / m * l; l = 0, m
+print(np.sort(x))
+print(np.sort(y))
+print(np.sort(z))
+n = 100
+xnew = np.linspace(np.min(x), np.max(x), n)
+ynew = np.linspace(np.min(y), np.max(y), n)
+znew = []
+# znew = [bernstein(x, y, z, i, j) for i in xnew for j in ynew]
+for i in xnew:
+    for j in ynew:
+        znew.append(bernstein(x, y, z, i, j))
+print(znew)
+plt.plot(x, y, 'o', xnew, ynew) # TODO построить 3д график, т.к. это поверхность
+plt.grid(True)
+plt.show()
+
