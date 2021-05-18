a99938fc27bdcd7e0bb8d71b45129f228f4e75e4
KartoonYoko <crashed9001@gmail.com>
2021-05-18 09:24:21 +0900

    asdad

--- Experimental data processing/lab 8/index.py
+++ Experimental data processing/lab 8/index.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import math
 import numpy as np
-
+from sympy import *
+from sympy.solvers.solveset import linsolve
 # variant 17
 # x = 1.5 y = 0.43
 
@@ -17,10 +18,6 @@ y = [-2.11, -2.33, -0.14, 0.43, 1.34, 2.65, 6.23, 9.23, 7.65, 4.23]
 
 # -2.11 - 0.5 * a11 +  0.5^2 * a12 = -2.33
 # https://overcoder.net/q/668685/%D0%BA%D0%B0%D0%BA-%D1%80%D0%B5%D1%88%D0%B8%D1%82%D1%8C-%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%83-%D0%BB%D0%B8%D0%BD%D0%B5%D0%B9%D0%BD%D1%8B%D1%85-%D1%83%D1%80%D0%B0%D0%B2%D0%BD%D0%B5%D0%BD%D0%B8%D0%B9-%D0%B2-sympy
-# a = []
-# a.append([-2.11, 0, 0])
-# for i in range(len(x)):
-#     pass
 
 
 # Структура, описывающая сплайн на каждом сегменте сетки
@@ -35,6 +32,9 @@ class SplineTuple:
     def print_spline(self):
         print('{a} + {x} * {b} + {x}^2 * {c} = {y}'.format(a=self.a, b=self.b, c=self.c, y=self.y, x=self.x))
 
+    def get_spline(self):
+        return '{a} + {x} * {b} + {x} ** 2 * {c} - {y}'.format(a=self.a, b=self.b, c=self.c, y=self.y, x=self.x)
+
 
 def BuildSpline(x, y, n):
     """
@@ -43,56 +43,42 @@ def BuildSpline(x, y, n):
         # y - значения функции в узлах сетки
         # n - количество узлов сетки
     """
-    # количество ур-ий для вычесления сплайнов
-    amount = (n - 1) * 2 - 1
-    # Инициализация массива функций для вычисления сплайнов
-    splines = [SplineTuple("none", "none", "none", "none", "none") for _ in range(0, amount)]
+    # количество сплайнов
+    amount = (n - 1)
+    # Инициализация массива функций для вычисления сплайнов (их в2 раза больше чем сплайнов)
+    splines = [SplineTuple("none", "none", "none", "none", "none") for _ in range(0, amount * 2)]
+    # массив для слау (понадобится при вычислении)
+    sp_lin = []
 
-    # т.к. x и y повторяются
-    buf = 2
-    count = 0
-    for i in range(0, amount):
-        if (i > 1) and (i < amount - 1):
-            if count == 1:
-                splines[i].x = x[buf + 1]
-                splines[i].y = y[buf + 1]
+    for i in range(amount):
+        for j in range(2):
+            if j == 1:
+                splines[i * 2 + j].x = x[i + 1]
+                splines[i * 2 + j].y = y[i + 1]
             else:
-                splines[i].x = x[buf]
-                splines[i].y = y[buf]
-            splines[i].a = "a" + str(buf) + "0"
-            splines[i].b = "a" + str(buf) + "1"
-            splines[i].c = "a" + str(buf) + "2"
-        else:
-            # для первых двух случаев и последнего
-            # для всего остального - мастеркард
-            kef = i
-            if i == amount - 1:
-                kef = buf
-            splines[i].x = x[kef]
-            splines[i].y = y[kef]
-            kef += 1
-            if count == 1:
-                kef -= 1
-                buf -= 1
-            splines[i].a = "a" + str(kef) + "0"
-            splines[i].b = "a" + str(kef) + "1"
-            splines[i].c = "a" + str(kef) + "2"
+                splines[i * 2 + j].x = x[i]
+                splines[i * 2 + j].y = y[i]
 
-        count += 1
-        if count == 2:
-            count = 0
-            buf += 1
-        splines[i].print_spline()
-    # splines[0].a = str(y[0])
-    # splines[0].b = "0"
-    # splines[0].c = "0"
-    # splines[0].y = str(y[0])
-    # TODO посчитать вручную первый сплайн
-    pass
+            splines[i * 2 + j].a = "a" + str(i + 1) + "0"
+            splines[i * 2 + j].b = "a" + str(i + 1) + "1"
+            splines[i * 2 + j].c = "a" + str(i + 1) + "2"
 
+            sp_lin.append(splines[i * 2 + j].get_spline())
+
+    # заполним массивы для решения СЛАУ
+    sp_symbols = []
+    for i in range(amount):
+        sp_symbols.append("a" + str(i + 1) + "0, " + "a" + str(i + 1) + "1, " + "a" + str(i + 1) + "2")
+    arr = []
+    arr = symbols(sp_symbols)
+    for key in splines:
+        key.print_spline()
+    print(linsolve(sp_lin, ))
     return splines
 
 
 x = [0, 0.25, 0.5, 0.75, 1]
 y = [2, 3, 5, 4, 6]
 BuildSpline(x, y, len(x))
+p = ()
+print(type(p))
